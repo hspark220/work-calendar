cb84ac0cae34980edc6eafc99713334df27dc417
Heon Soo Park <hspark220@gmail.com>
2023-08-10 20:11:22 -0400

    Initialize div container

---
+++ src/components/Calendar.js
@@ -0,0 +1,19 @@
+import Day from './Day';
+
+const Calendar = () => {
+
+
+
+    return (
+        <div className='calendar' style={{
+            display: "grid",
+            width: "1000px",
+            height: "500px",
+            border: "1px solid"
+          }}>
+            
+        </div>
+    );
+}
+
+export default Calendar;
---
+++ src/components/Day.js
@@ -0,0 +1,16 @@
+
+
+const Day = () => {
+
+
+    return (
+        <div className='day' style={{
+            width: "100px",
+            height: "100px",
+            border: "1px solid"
+          }}>
+          </div>
+    )
+}
+
+export default Day;
